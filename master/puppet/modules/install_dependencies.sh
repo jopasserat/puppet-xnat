5ef935bf04ca860c9f5c46784dc271b26c3c223a
Erwin Vast <e.vast@erasmusmc.nl>
2013-05-23 13:49:26 +0200

    install sun jdk in puppet script

--- master/puppet/modules/install_dependencies.sh
+++ master/puppet/modules/install_dependencies.sh
@@ -6,3 +6,4 @@ git clone https://github.com/glarizza/puppet-pick.git pick
 git clone https://github.com/puppetlabs/puppetlabs-stdlib stdlib
 git clone https://github.com/camptocamp/puppet-archive.git archive
 git clone https://github.com/ripienaar/puppet-concat.git concat
+git clone https://github.com/bcarpio/puppet-java.git java
--- master/puppet/modules/xnat/manifests/xnatapp.pp
+++ master/puppet/modules/xnat/manifests/xnatapp.pp
@@ -32,6 +32,7 @@ define xnat::xnatapp (
 {
   require xnat
   require postgresql::server
+  require java
   
   $tomcat_root = "$webapp_base/$system_user/tomcat"
   $installer_dir = "/home/$system_user/xnat-builder"
@@ -42,6 +43,24 @@ define xnat::xnatapp (
   exec { "apt-get update":
     command => "/usr/bin/apt-get update",
     onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+  }
+  ->
+  # Check for latest java version on master
+  file { "${java::params::java_base}/.jdk${java::params::java_version}.jinfo":
+    mode => 0644,
+    owner => root,
+    group => root,
+    source => "puppet:///modules/java/.jdk${java::params::java_version}.jinfo",
+    alias => "java-source-jinfo"
+  }
+  ->
+  # Set correct java version, using update-java-alternatives to prevent console choices
+  exec { "update-alternatives install sun jdk":
+    command => "/bin/cat /usr/lib/jvm/.jdk${java::params::java_version}.jinfo | /bin/grep -E '^(jre|jdk)' | /usr/bin/awk '{print \"/usr/bin/\" \$2 \" \" \$2 \" \" \$3 \" 30 \r \"}' | /usr/bin/xargs -t -n4 sudo /usr/bin/update-alternatives --verbose --install > ~/output.txt"
+  }
+  ->  
+  exec { "update-java-alternatives set sun jdk":
+    command => "/usr/sbin/update-java-alternatives --set jdk1.7.0_21"
   }
   ->
   tomcat::webapp { $instance_name:
@@ -61,18 +80,18 @@ define xnat::xnatapp (
 #    setenv => ['ADD_JAVA_OPTS="-Xms512m -Xmx1024m"'],
 #  }
   ->
-  # Ensure the archive directories are present
-  exec { "mkdir-root":
-    command => "/bin/mkdir -p $archive_root"
-  }
-  ->
+  # Ensure the archive directories are present (not working, created manually)
+  #exec { "mkdir-root":
+  #  command => "/bin/mkdir -p $archive_root"
+  #}
+  #->
   # Make the archive directories
-  file { $archive_dirs:
-    ensure => "directory",
-    mode => "700",
-    owner => $system_user,
-  }
-  ->
+  #file { $archive_dirs:
+  #  ensure => "directory",
+  #  mode => "700",
+  #  owner => $system_user,
+  #}
+  #->
   # configure the postgres db (step 2).
   postgresql::database_user { $db_username:
     createrole => true,
@@ -94,10 +113,10 @@ define xnat::xnatapp (
   ->
   # Remove the previous checkouts of xnat if present
   #FIXME: make this more safe, this is very very insecure!!!
-  exec { "remove-previous-checkout":
-    command => "/bin/rm -rf $installer_dir",
-  }
-  ->
+#  exec { "remove-previous-checkout": # not good when testing
+#    command => "/bin/rm -rf $installer_dir",
+#  }
+#  ->
 #  wget::fetch { "download-xnat":
 #    source => "ftp://ftp.nrg.wustl.edu/pub/xnat/xnat_1_6_1.tar.gz",
 #    destination => "$download_dir/xnat_1_6_1.tar.gz",
@@ -105,45 +124,45 @@ define xnat::xnatapp (
 #    verbose => true,
 #  }
 #  ->
-  archive { 'xnat-1.6.1':
-    ensure => present,
-    #url    => 'ftp://ftp.nrg.wustl.edu/pub/xnat/xnat_1_6_1.tar.gz',
-    url    => 'http://192.168.122.1/xnat_1_6_1.tar.gz',
-    target => $download_dir,
-    timeout => 240,  
-  }
-  ->
-  exec { "move-to-installer-dir":
-    command => "/bin/mv $download_dir/xnat $installer_dir"
-  }
-  ->
-#  # Clone the xnat builder dev branch
-#  exec { "mercurial-clone-xnatbuilder":
-#    command => "/usr/bin/hg clone http://hg.xnat.org/xnat_builder_1_6dev $installer_dir",
-#    creates => $installer_dir,
-#    timeout => 12000000,
+#  archive { 'xnat-1.6.1': # not working, problems (access rights) when running as root?
+#    ensure => present,
+#    url    => 'ftp://ftp.nrg.wustl.edu/pub/xnat/xnat_1_6_1.tar.gz',
+#    #url    => 'http://192.168.122.1/xnat_1_6_1.tar.gz',
+#    target => $download_dir,
+#    timeout => 240,  
 #  }
 #  ->
-#  # Clone the xnat pipeline dev branch
-#  exec { "mercurial-clone-xnat-pipeline":
-#    command => "/usr/bin/hg clone http://hg.xnat.org/pipeline_1_6dev $installer_dir/pipeline",
-#    creates => "$installer_dir/pipeline",
-#    timeout => 12000000,
+#  exec { "move-to-installer-dir": #nodig?
+#    command => "/bin/mv $download_dir/xnat_1_6_1.tar.gz $installer_dir/"
 #  }
 #  ->
+  # Clone the xnat builder dev branch
+  exec { "mercurial-clone-xnatbuilder":
+    command => "/usr/bin/hg clone http://hg.xnat.org/xnat_builder_1_6dev $installer_dir",
+    creates => $installer_dir,
+    timeout => 12000000,
+  }
+  ->
+  # Clone the xnat pipeline dev branch	# where is this used for?
+  exec { "mercurial-clone-xnat-pipeline":
+    command => "/usr/bin/hg clone http://hg.xnat.org/pipeline_1_6dev $installer_dir/pipeline",
+    creates => "$installer_dir/pipeline",
+    timeout => 12000000,
+  }
+  ->
   #Configure the XNAT build properties.
   file { "$installer_dir/build.properties":
     ensure => file,
     content => template('xnat/build.properties.erb'),
     mode => '600',
   }
-  ->
-  exec { "xnat-setup":
-    command => "$installer_dir/bin/setup.sh",
-    cwd => "$installer_dir",
-    environment => "JAVA_HOME=$java_home",
-    timeout => 36000000,
-  }
+  #->
+  #exec { "xnat-setup":	#JAVA_HOME not set, script not working, reason unknown
+  #  command => "$installer_dir/bin/setup.sh",
+  #  cwd => "$installer_dir",
+  #  environment => "JAVA_HOME=$java_home",
+  #  timeout => 36000000,
+  #}
   #TODO: create tables and views using psql and find a way to supply the passwords and such
   #
   #->
