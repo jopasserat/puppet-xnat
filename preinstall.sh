2992de08bc840249965be40bdc925739837edbe8
Erwin Vast <root@xnattcia.erasmusmc.nl>
2013-07-16 15:02:54 +0200

    export java home, facter does not work, except when calling puppet twice. Reason unknown.

--- preinstall.sh
+++ preinstall.sh
@@ -8,3 +8,4 @@ sh install_dependencies.sh
 cp xnat/.jdk1.7.0_21.jinfo /usr/lib/jvm/
 echo "export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_21\nexport JAVA_OPTS=\"-Xms512m -Xmx1024m\"" > /usr/share/tomcat7/bin/setenv.sh
 puppet apply xnat/tests/test.pp > /puppet.out
+export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_21
