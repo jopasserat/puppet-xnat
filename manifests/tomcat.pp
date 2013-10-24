e523bd4a18835f1a20e710da6e2c72d8d3087459
Erwin Vast <root@localhost.localdomain>
2013-10-24 12:49:22 +0200

    bugfix tomcat dir existing

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -5,12 +5,12 @@ class tomcat {
 
   exec { "download tomcat":
     command => "wget -P /tmp/ http://apache.mirror.1000mbps.com/tomcat/tomcat-7/v7.0.42/bin/apache-tomcat-7.0.42.tar.gz",
-    unless => "test -d /usr/share/tomcat7/"
+    unless => "test -d /usr/share/tomcat7/bin/"
   } ->
-   
+  
   exec { "extract tomcat":
     command => "tar -xzf /tmp/apache-tomcat-7.0.42.tar.gz -C /usr/share/;mv /usr/share/apache-tomcat-7.0.42 /usr/share/tomcat7/",
-    unless => "test -d /usr/share/tomcat7/"
+    unless => "test -d /usr/share/tomcat7/bin/"
   } ->
 
   exec { "set permissions":
--- preinstall.sh
+++ preinstall.sh
@@ -6,6 +6,5 @@ mv xnat/install_dependencies.sh .
 sh install_dependencies.sh
 mkdir -p /usr/lib/jvm/
 cp xnat/.jdk1.7.0_21.jinfo /usr/lib/jvm/
-mkdir -p /usr/share/tomcat7/bin/
 export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_21
 puppet apply xnat/tests/test.pp &> /puppet.out
