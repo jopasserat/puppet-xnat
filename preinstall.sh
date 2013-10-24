f55cb16ca472e8218137f8eea38cb34ed536f821
Erwin Vast <root@localhost.localdomain>
2013-10-24 11:09:37 +0200

    bugfix mkdir preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -4,8 +4,8 @@ cd /etc/puppet/modules/
 gem install puppet-module
 mv xnat/install_dependencies.sh .
 sh install_dependencies.sh
-mkdir /usr/lib/jvm/
+mkdir -p /usr/lib/jvm/
 cp xnat/.jdk1.7.0_21.jinfo /usr/lib/jvm/
-mkdir /usr/share/tomcat7/bin/
+mkdir -p /usr/share/tomcat7/bin/
 export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_21
 puppet apply xnat/tests/test.pp &> /puppet.out
