4972f12e54d5e62fe5edb4f5fd8d094dae9e73a3
Erwin Vast <root@localhost.localdomain>
2013-10-24 14:26:48 +0200

    bugfix new version postgresql

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,6 @@ puppet module install jgoettsch/mercurial
 puppet module install puppetlabs/postgresql
 puppet module install maestrodev/wget
 git clone https://github.com/glarizza/puppet-pick.git pick
-git clone https://github.com/puppetlabs/puppetlabs-stdlib stdlib
+#git clone https://github.com/puppetlabs/puppetlabs-stdlib stdlib
 git clone https://github.com/camptocamp/puppet-archive.git archive
-git clone https://github.com/ripienaar/puppet-concat.git concat
+#git clone https://github.com/ripienaar/puppet-concat.git concat
