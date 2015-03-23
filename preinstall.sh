d53f0b0dde4885ca296b41bc48daa1810f03d768
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2015-03-23 13:59:05 +0000

    add: Keypair generation script. Ignore generated files and call script in preinstallation

--- preinstall.sh
+++ preinstall.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 cd /etc/puppet/modules/
-#gem install puppet-module
-#mv xnat/install_dependencies.sh .
+
+sh generate_keypair.sh
 sh xnat/install_dependencies.sh
-#mkdir -p /usr/lib/jvm/
-#cp xnat/.jdk1.7.0_60.jinfo /usr/lib/jvm/
-#export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_60
+
 puppet apply xnat/tests/test.pp &> /puppet.out
+
