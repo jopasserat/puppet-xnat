715c20e7c0fe2f6d74f5855aecd920cc858330ba
filippomortari <filippo.mortari@gmail.com>
2015-05-15 17:26:20 +0100

    enh: Allow passing base directory for key/certs destination

--- generate_keypair.sh
+++ generate_keypair.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir -p files/etc/ssl/{certs,private}
-openssl genrsa -out files/etc/ssl/private/xnat.key 1024
-openssl req -new -key files/etc/ssl/private/xnat.key -out /tmp/xnat.csr -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
-openssl x509 -req -days 3650 -in /tmp/xnat.csr -signkey files/etc/ssl/private/xnat.key -out files/etc/ssl/certs/xnat.crt
+basedir=$1
+
+mkdir -p ${basedir}/files/etc/ssl/{certs,private}
+openssl genrsa -out ${basedir}/ssl/private/xnat.key 1024
+openssl req -new -key ${basedir}/ssl/private/xnat.key -out /tmp/xnat.csr -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
+openssl x509 -req -days 3650 -in /tmp/xnat.csr -signkey ${basedir}/ssl/private/xnat.key -out ${basedir}/ssl/certs/xnat.crt
 rm /tmp/xnat.csr
 
