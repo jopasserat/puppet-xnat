31808c257fbb4b03c01184be3eaf4534fc89beb2
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2015-05-16 15:52:32 +0100

    fix: Key generation path + report changes to preinstall script

--- generate_keypair.sh
+++ generate_keypair.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-basedir=$1
+basedir=$1/files
 
-mkdir -p ${basedir}/files/etc/ssl/{certs,private}
-openssl genrsa -out ${basedir}/ssl/private/xnat.key 1024
-openssl req -new -key ${basedir}/ssl/private/xnat.key -out /tmp/xnat.csr -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
-openssl x509 -req -days 3650 -in /tmp/xnat.csr -signkey ${basedir}/ssl/private/xnat.key -out ${basedir}/ssl/certs/xnat.crt
+mkdir -p ${basedir}/etc/ssl/{certs,private}
+openssl genrsa -out ${basedir}/etc/ssl/private/xnat.key 1024
+openssl req -new -key ${basedir}/etc/ssl/private/xnat.key -out /tmp/xnat.csr -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
+openssl x509 -req -days 3650 -in /tmp/xnat.csr -signkey ${basedir}/etc/ssl/private/xnat.key -out ${basedir}/etc/ssl/certs/xnat.crt
 rm /tmp/xnat.csr
 
--- preinstall.sh
+++ preinstall.sh
@@ -2,7 +2,7 @@
 
 cd /etc/puppet/modules/
 
-sh generate_keypair.sh
+sh xnat/generate_keypair.sh /etc/puppet/modules/xnat
 sh xnat/install_dependencies.sh
 
 puppet apply xnat/tests/test.pp &> /puppet.out
