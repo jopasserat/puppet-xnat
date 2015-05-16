7ae55d745c3cee1df60de27cde5de87a22016f18
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2015-05-16 12:25:02 +0100

    fix: temporarily force camptocamp-archive module to lower version as >= 0.6.x breaks with remote URLs

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-MODULES_TO_INSTALL="puppetlabs-ruby jgoettsch-mercurial puppetlabs-postgresql puppetlabs-rsync maestrodev-wget camptocamp-archive puppetlabs-java stahnma-epel jfryman-nginx puppetlabs-tomcat"
+MODULES_TO_INSTALL="puppetlabs-ruby jgoettsch-mercurial puppetlabs-postgresql puppetlabs-rsync maestrodev-wget puppetlabs-java stahnma-epel jfryman-nginx puppetlabs-tomcat"
 MODULES_INSTALLED=`puppet module list`
 
 for i in ${MODULES_TO_INSTALL}; do
   # only install module if needed
   echo ${MODULES_INSTALLED} | grep -q $i || puppet module install $i
 done
+
+# temporarily forced to lower version => v >= 0.6.x does not work with remote URLs
+echo ${MODULES_INSTALLED} | grep -q camptocamp-archive || puppet module install --version 0.5.3 camptocamp-archive
 
