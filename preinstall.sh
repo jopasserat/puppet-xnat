ab314339e660b828c2718047caad2681369b0621
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2015-05-16 18:39:53 +0100

    fix: Explicit call to bash as sh might point to another shell without the same support

--- preinstall.sh
+++ preinstall.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /etc/puppet/modules/
 
-sh xnat/generate_keypair.sh /etc/puppet/modules/xnat
-sh xnat/install_dependencies.sh
+bash xnat/generate_keypair.sh /etc/puppet/modules/xnat
+bash xnat/install_dependencies.sh
 
 puppet apply xnat/tests/test.pp &> /puppet.out
 
